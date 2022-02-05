e8f6dffa4f255f7c5cf95bd0e69e8d6d39bc0ead
ERICMILLER12 <>
2022-02-05 03:59:17 +0000

    Explore2 done

--- Explore2.cpp
+++ Explore2.cpp
@@ -8,15 +8,19 @@ struct Point {
 };
 Point::Point() {
 	// Normally, you do not print in a constructor
-	cout << "In the constructor" << endl;
 	x = 0;
 	y = 0;
 }
 
 int main() {
 
+  cout << "Creating a" << endl;
+	Point a;
+	cout << "Creating b" << endl;
+	Point b;
 
-
+	cout << "Point a: x = " << a.x << ", y = " << a.y << endl;
+	cout << "Point b: x = " << b.x << ", y = " << b.y << endl;
 
 	return 0;
 }
